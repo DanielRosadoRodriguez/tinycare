0c6c181dde50f79e154ae0a985431f656a3760c6
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-10-26 16:50:12 -0600

    feat(pages): implementa landing page base con Bootstrap

--- tinycare/settings.py
+++ tinycare/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Nuestra app de páginas estáticas (Landing, About, etc.)
+    'pages',
     # Accounts (módulo de autenticación y registro de usuarios)
     'accounts',
 ]
