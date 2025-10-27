59146ed522a3f9e2b94be8349e1b041d8c492164
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-10-26 20:08:42 -0600

    feat(api): agrega DRF en settings para exponer API REST

--- tinycare/settings.py
+++ tinycare/settings.py
@@ -47,10 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Nuestra app de páginas estáticas (Landing, About, etc.)
-    'pages',
-    # Accounts (módulo de autenticación y registro de usuarios)
-    'accounts',
+    'rest_framework',  # [ADD] DRF
+    'pages', # Nuestra app de páginas estáticas (Landing, About, etc.)
+    'accounts', # Accounts (módulo de autenticación y registro de usuarios)
 ]
 
 MIDDLEWARE = [
