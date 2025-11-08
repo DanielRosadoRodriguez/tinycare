e469b71932dbb827c8eb2a1837a40a669031cb05
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-11-07 23:27:45 -0600

    feat (seguridad): implementa función de descargar datos personales para cumplir la norma

--- privacy/urls.py
+++ privacy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from privacy.views import privacy_notice_view, actualizar_consentimientos_view
+from privacy.views import privacy_notice_view, actualizar_consentimientos_view, download_personal_data
 
 app_name = "privacy"
 
 urlpatterns = [
     path("", privacy_notice_view, name="notice"),
     path("consentimientos/", actualizar_consentimientos_view, name="actualizar_consentimientos"),
+    path("descargar-datos/", download_personal_data, name="download_data"),
 ]
